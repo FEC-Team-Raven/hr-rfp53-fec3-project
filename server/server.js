4520640865265c5e9845995bedafb387f60bea29
Jacky Chen <jacky.chen0319@gmail.com>
2021-06-05 12:55:27 -0700

    Updated server with get methods, applied eslint

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const token = require('./config');
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
@@ -12,4 +17,20 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
+});
+
+// Retrieves the list of products
+app.get('/products', (req, res) => {
+  axios({
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((res) => {
+      res.end(res.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
