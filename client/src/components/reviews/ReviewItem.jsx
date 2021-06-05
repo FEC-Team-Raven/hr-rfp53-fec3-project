1e4a331cfad0c968044e7d52bd0e1e00105ec2ad
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-06-04 17:22:06 -0700

    Added default product to our app to be passed down to child components

---
+++ client/src/components/reviews/ReviewItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ReviewItem = (props) => (
+  <div>
+    Single Review: {props.review}
+  </div>
+);
+
+export default ReviewItem;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,50 @@
 const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const token = require('./config.js');
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log(`SERVING GET REQUEST AT ${req.url}`);
+  axios({
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+    headers: {
+      Authorization: token
+    }
+  })
+    .then(result => {
+      res.end(result.data[0]);
+    })
+    .catch(err => {
+      console.log(err);
+      res.end();
+    });
+});
+
+app.get('/products', (req, res) => {
+  console.log(`SERVING GET REQUEST AT ${req.url}`);
+  axios({
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+    headers: {
+      Authorization: token
+    }
+  })
+    .then(result => {
+      res.end(JSON.stringify(result.data));
+    })
+    .catch(err => {
+      console.log(err);
+      res.end();
+    });
+});
+
+app.get('/reviews', (req, res) => {
+  console.log(`SERVING GET REQUEST AT ${req.url}`);
+
 });
 
 app.listen(port, () => {
