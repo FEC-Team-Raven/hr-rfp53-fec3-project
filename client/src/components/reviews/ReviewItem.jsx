62f85a8f70bb184bdeec59caa4d380b9c036152b
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-06-07 11:57:41 -0700

    Implemented getting reviews from api. Started building out ReviewItem

--- client/src/components/reviews/ReviewItem.jsx
+++ client/src/components/reviews/ReviewItem.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+var parseDate = (date) => {
+  var date = new Date(date);
+  var options = {year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+};
+
 const ReviewItem = (props) => (
   <div>
-    Single Review: {props.review}
+    {console.log(props.review)}
+    <div className="reviewRow">
+      <div>Stars: {props.review.rating}</div>
+      <div>{props.review.reviewer_name}, {parseDate(props.review.date)}</div>
+    </div>
   </div>
 );
 
