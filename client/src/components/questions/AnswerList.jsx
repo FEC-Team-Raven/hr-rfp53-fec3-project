32ce3758af78eeab766ef6f3d56f724db1c5168c
Will McCulloch <willdmcculloch@gmail.com>
2021-06-10 13:59:16 -0700

    implement load more answers and hide answers functionality;

--- client/src/components/questions/AnswerList.jsx
+++ client/src/components/questions/AnswerList.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AnswerItem from './AnswerItem.jsx';
 
 const AnswerList = (props) => {
+  const [showAll, setShowAll] = useState(false);
   let answers = Object.entries(props.value);
   let showA = answers.length === 0;
+  let ans = answers.length > 2;
+  let ansDefault = ans ? answers.slice(0, 2) : answers;
+  let show = showAll ? answers : ansDefault;
+
+  const onClick = () => {
+    setShowAll(!showAll);
+  };
   return (
     <div className="answer">
       {showA ? <div></div> : <div>A: </div>}
       <div>
-        {answers.map((item) =>
+        {show.map((item) =>
           <AnswerItem value={item} getAnswer={props.getAnswer}/>
         )}
       </div>
+      {ans ? <div><button onClick={onClick}>
+        {showAll ? 'HideAnswers' : 'Load more Answers'}
+      </button></div> : <div></div>}
     </div>
   );
 };
--- client/src/components/questions/QuestionsList.jsx
+++ client/src/components/questions/QuestionsList.jsx
@@ -6,10 +6,10 @@ import QuestionItem from './QuestionItem.jsx';
 const QuestionsList = (props) => {
   const productId = useContext(ProductContext);
   const question = useContext(QuestionContext);
-
+  const questionShort = question.length > 4 ? question.slice(0, 4) : question;
   return (
     <div>
-      {question.map((item) =>
+      {questionShort.map((item) =>
         <QuestionItem value={item} id={productId} getQuestions={props.getQuestions}/>
       )}
     </div>
