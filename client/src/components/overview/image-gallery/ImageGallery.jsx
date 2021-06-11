5faac90ec92d6c828b6dbb1d1f5969d5d40247c5
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-06-11 13:49:40 -0700

    Implemented rendering images in ImageGallery thumbnails

--- client/src/components/overview/image-gallery/ImageGallery.jsx
+++ client/src/components/overview/image-gallery/ImageGallery.jsx
@@ -2,17 +2,26 @@ import React, { useEffect } from 'react';
 import Thumbnail from './../shared/Thumbnail.jsx';
 
 const ImageGallery = props => {
+  const images = [
+    'https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xvdGhpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1445205170230-053b83016050?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2xvdGhpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1560243563-062bfc001d68?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2xvdGhpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1525562723836-dca67a71d5f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNsb3RoaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1571867424485-369464ed33cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1479064555552-3ef4979f8908?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNsb3RoaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1524275539700-cf51138f679b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNsb3RoaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+  ];
 
   return (
     <div id="image-gallery">
       <div id="image-gallery-thumbnail-list">
-        <Thumbnail selected={true}/>
-        <Thumbnail />
-        <Thumbnail />
-        <Thumbnail />
-        <Thumbnail />
-        <Thumbnail />
-        <Thumbnail />
+        <Thumbnail imageURL={images[0]} selected={true}/>
+        <Thumbnail imageURL={images[1]}/>
+        <Thumbnail imageURL={images[2]}/>
+        <Thumbnail imageURL={images[3]}/>
+        <Thumbnail imageURL={images[4]}/>
+        <Thumbnail imageURL={images[5]}/>
+        <Thumbnail imageURL={images[6]}/>
       </div>
       Image Gallery
     </div>
--- client/src/components/overview/shared/Thumbnail.jsx
+++ client/src/components/overview/shared/Thumbnail.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 
 const Thumbnail = props => {
+  let selected = props.selected;
   const select = event => {
+    if (selected) { selected = false; }
+    console.log('Clickd!');
     document.querySelector('#image-gallery-thumbnail-list').querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
   };
 
   return (
-    <button className={`thumbnail ${props.selected ? 'selected' : ''}`} onClick={select}></button>
+    <button className={`thumbnail ${selected ? 'selected' : ''}`} onClick={select}>
+      <div className="thumbnail-img" style={{'backgroundImage': `url(${props.imageURL})`}}></div>
+    </button>
   );
 };
 
