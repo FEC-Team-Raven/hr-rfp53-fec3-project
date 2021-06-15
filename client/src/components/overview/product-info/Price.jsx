61bf6517a3333eba801761b8b75600848da88bf5
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-06-15 15:37:16 -0700

    Implemented rendering of sale price in Price component

--- client/src/components/overview/product-info/Price.jsx
+++ client/src/components/overview/product-info/Price.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Price = props => (
+const Price = props => props.salePrice !== null ? (
   <div id="price">
-    ${props.price}
+    <span id="sale-price">${props.salePrice}</span><span id="original-price">${props.price}</span>
   </div>
+) : (
+  <div id="price">${props.price}</div>
 );
 
 export default Price;
