ed8efdcf7b00353a3b3b8bfc4e11f2fa8f00db8f
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-06-17 13:56:53 -0700

    Added pem key to git ignore

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,35 +6,37 @@ import Related from './components/related/Related.jsx';
 import Questions from './components/questions/Questions.jsx';
 import Reviews from './components/reviews/Reviews.jsx';
 
+
 const App = () => {
-  const [productId, setProductId] = useState(0);
-  const [productName, setProductName] = useState('');
-  const [loading, setLoading] = useState(true);
+  const [ productData, setProductData ] = useState(0);
+  const [ productIDtoRender, setProductIDtoRender ] = useState(17067);
+  const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
     if (loading) {
-      setLoading(false);
-      axios('http://localhost:3000/products')
+      axios(`http://localhost:3000/products/${productIDtoRender}`)
         .then(products => {
-          console.log('id', products.data[0].id);
-          console.log('name', products.data[0].name);
-
-          setProductId(products.data[0].id);
-          setProductName(products.data[0].name);
+          setProductData(products.data);
+          setLoading(false);
         })
         .catch(err => {
           console.log(err);
-          setLoading(true);
         });
     }
-  });
+  }, []);
+
+  if (loading) {
+    return (
+      <div>Loading product page...</div>
+    );
+  }
 
   return (
     <div>
-      <Overview productId={productId}/>
-      {/* <Related productId={productId}/> */}
-      <Questions productId={productId} productName={productName}/>
-      <Reviews productId={productId}/>
+      <Overview productId={productData.id} product={productData}/>
+      <Related productId={productData.id}/>
+      <Questions productId={productData.id} productName={productData.name}/>
+      <Reviews productId={productData.id}/>
     </div>
   );
 };
