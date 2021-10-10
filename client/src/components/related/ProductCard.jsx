ebd756ae5a6cff75b6536dcb80f3515e53414ffb
Jacky Chen <jacky.chen0319@gmail.com>
2021-10-10 04:30:20 -0700

    Implemented navigation functionality to carousel

--- client/src/components/related/ProductCard.jsx
+++ client/src/components/related/ProductCard.jsx
@@ -67,12 +67,7 @@ const ProductCard = ({ currProductId, productId, list }) => {
     }
   }, []);
 
-  // console.log('product data:', productData);
-  // console.log('styles data:', stylesData);
-  // console.log('currProductData:', currProductData);
-  // console.log('comparedProductData:', comparedProductData);
-
-  // Retrieves thumbnail url
+  // Retrieves thumbnail url. By default, thumbnail the missing picture png
   let thumbnail = sampleImg;
   if (stylesData.results) {
     thumbnail = stylesData.results[0].photos[0].thumbnail_url;
@@ -121,81 +116,3 @@ const ProductCard = ({ currProductId, productId, list }) => {
 };
 
 export default ProductCard;
-
-// <div className="carousel__photo">
-//   <img className="carousel__photo" src={thumbnail}></img>
-//   <div>button</div>
-//   <div>product info
-//     <div>{productData.category}</div>
-//     <div>{productData.name}</div>
-//     <div>price</div>
-//     <div>rating</div>
-//   </div>
-// </div>
-
-// // Rating calculator helper function
-// const computeRating = () => {
-//   let ratingObj = product.rating;
-//   let totalRatings = 0;
-//   let numberOfRatings = 0;
-//   for (var score in ratingObj) {
-//     totalRatings += (Number(score) * Number(ratingObj[score]));
-//     numberOfRatings += Number(ratingObj[score]);
-//   }
-//   return totalRatings / numberOfRatings;
-
-// };
-// let rating = computeRating();
-
-// let styles = product.results;
-// let previewImg, defaultPrice, salePrice;
-
-// // Renders product price and default image
-// const renderProductInfo = () => {
-//   for (let i = 0; i < styles.length; i++) {
-//     if (styles[i]['default?']) {
-//       previewImg = styles[i].photos[0].url;
-//       defaultPrice = '$' + styles[i].original_price;
-//       if (salePrice) {
-//         defaultPrice = '<del>' + defaultPrice + '</del>';
-//       }
-//       break;
-//     } else if (!styles[styles.length - 1]['default?']) {
-//       defaultPrice = 'N/A';
-//     }
-//   }
-// };
-// renderProductInfo();
-
-// // If missing image, then use placeholder image
-// if (!previewImg) {
-//   previewImg = sampleImg;
-// }
-
-// // Renders action button, depending on which list the button appears within
-// const renderActionBtn = () => {
-//   if (list === 'related') {
-//     return (
-//       <RelatedActionButton
-//         product={product} list={list}/>
-//     );
-//   } else {
-//     return (
-//       <OutfitActionButton
-//         product={product} list={list}/>
-//     );
-//   }
-// };
-
-// return (
-//   <div id={product.product_id} className="card">
-//     <img className="preview" src={previewImg} width="250" height="300"></img>
-//     {renderActionBtn()}
-//     <div className="category">CATEGORY
-//       <div id="product-name">{product.name}</div>
-//       <div id="product-price">{defaultPrice}</div>
-//       <div>{salePrice}</div>
-//       <Stars rating={rating}/>
-//     </div>
-//   </div>
-// );
--- client/src/components/related/RelatedProducts.jsx
+++ client/src/components/related/RelatedProducts.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const RelatedProducts = ({ currProductId }) => {
   const [ relatedIds, setRelatedIds ] = useState([]);
+  // const [ relatedImgIndex, setRelatedImgIndex] = useState(1);
   const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
@@ -21,12 +22,43 @@ const RelatedProducts = ({ currProductId }) => {
     }
   }, []);
 
-  // Carousel functionality
+  // Carousel
+  const carouselImages = document.querySelectorAll('.card');
+  const carouselButtons = document.querySelectorAll('.carousel__button');
+  const numberOfImages = carouselImages.length;
+  let relatedImgIndex = 1;
+  let translateX = 0;
+
+  // Carousel navigation
+  carouselButtons.forEach(button => {
+    button.addEventListener('click', event => {
+      // Previous button
+      if (event.target.id === 'prev') {
+        if (relatedImgIndex !== 1) {
+          // setRelatedImgIndex(relatedImgIndex + 1);
+          relatedImgIndex--;
+          translateX += 258.7;
+        }
+      // Next button
+      } else {
+        if (relatedImgIndex < (numberOfImages - 3)) {
+          // setRelatedImgIndex(relatedImgIndex - 1);
+          relatedImgIndex++;
+          translateX -= 258.7;
+        }
+      }
+
+      console.log(relatedImgIndex);
+
+      carouselImages.forEach(image => {
+        image.style.transform = `translateX(${translateX}px)`;
+      });
+    });
+  });
 
   return (
-
     <div className="related">
-      <div className="carousel__button prev">&#x2190;</div>
+      <div id="prev" className="carousel__button">&#x2190;</div>
 
       <div className="carousel">
         <div className="carousel__images">
@@ -36,14 +68,10 @@ const RelatedProducts = ({ currProductId }) => {
               productId={productId}
               list={'related'}/>
           )}
-          {/* <ProductCard
-            currProductId={currProductId}
-            productId={relatedIds[0]}
-            list={'related'}/> */}
         </div>
       </div>
 
-      <div className="carousel__button next">&#x2192;</div>
+      <div id="next" className="carousel__button">&#x2192;</div>
 
     </div>
 
